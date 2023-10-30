72456dbeac5790bd2a980812011ea4de6adc1a79
Nienormalny <dawid.szadurski@gmail.com>
2023-10-30 09:39:49 +0100

    Close #1: Change tab title from Localhost to To-Do List

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import '@/styles/globals.css'
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-  <Header/>
-  <div className="container mx-auto min-h-screen">
-  <Component {...pageProps} />
-  </div>
-  <Footer/>
-  </>
-  )
+      <Head>
+        <title>To-Do List</title>
+      </Head>
+      <Header />
+      <div className='container mx-auto min-h-screen'>
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </>
+  );
 }
